278fc950bcbcd861f72068b617af11635555c24e
Karthik Ramasamy <karthinml@gmail.com>
2021-01-23 23:29:50 +0530

    AddEmployee: Added repository test and fixed test failures

--- employee-portal-service/src/test/java/com/kata/employee/employeeportalservice/service/EmployeeServiceTest.java
+++ employee-portal-service/src/test/java/com/kata/employee/employeeportalservice/service/EmployeeServiceTest.java
@@ -1,10 +1,16 @@
 package com.kata.employee.employeeportalservice.service;
 
+import com.kata.employee.employeeportalservice.model.Employee;
 import com.kata.employee.employeeportalservice.repository.EmployeeRepository;
 import org.junit.Test;
+
+import java.text.ParseException;
+
 import static com.kata.employee.employeeportalservice.helper.EmployeePortalServiceTestHelper.getSampleEmployeeData;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class EmployeeServiceTest {
 
@@ -12,7 +18,8 @@ public class EmployeeServiceTest {
     private final EmployeeService employeeService = new EmployeeService(employeeRepositoryMock);
 
     @Test
-    public void should_be_able_to_add_employee_to_employee_portal() {
+    public void should_be_able_to_add_employee_to_employee_portal() throws ParseException {
+        when(employeeRepositoryMock.save(any(Employee.class))).thenReturn(getSampleEmployeeData());
         assertEquals(getSampleEmployeeData(), employeeService.registerEmployee(getSampleEmployeeData()));
     }
 }
