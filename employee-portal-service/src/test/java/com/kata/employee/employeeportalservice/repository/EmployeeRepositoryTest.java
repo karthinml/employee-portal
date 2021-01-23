bbb59a63e31a39350cc1f24e72066e83d805fda6
Karthik Ramasamy <karthinml@gmail.com>
2021-01-24 01:01:00 +0530

    ListEmployees: Added unit test for get employees operation of employee repository

--- employee-portal-service/src/test/java/com/kata/employee/employeeportalservice/repository/EmployeeRepositoryTest.java
+++ employee-portal-service/src/test/java/com/kata/employee/employeeportalservice/repository/EmployeeRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.kata.employee.employeeportalservice.repository;
 
 import com.kata.employee.employeeportalservice.model.Employee;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.kata.employee.employeeportalservice.helper.EmployeePortalServiceTestHelper.getSampleEmployeeData;
 import static org.junit.Assert.assertEquals;
@@ -19,8 +24,33 @@ public class EmployeeRepositoryTest {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Before
+    public void setUp() {
+        employeeRepository.deleteAll();
+    }
+
     @Test
     public void should_be_able_to_add_employee_to_portal() throws ParseException {
         assertEquals(employeeRepository.save(getSampleEmployeeData()), getSampleEmployeeData());
+    }
+
+    @Test
+    public void should_get_empty_list_when_querying_for_employee_list() {
+        assertEquals(employeeRepository.findAllByOrderByFirstNameAsc(), new ArrayList<>());
+    }
+
+    @Test
+    public void should_get_employees_registered_in_portal_ascending_by_first_name() throws ParseException {
+        ArrayList<Employee> expected = new ArrayList<>();
+        expected.add(getSampleEmployeeData());
+        Employee employee = getSampleEmployeeData();
+        employee.setEmployeeId("1002");
+        employee.setFirstName("Arun");
+        expected.add(employee);
+        employeeRepository.saveAll(expected);
+        List<Employee> data = employeeRepository.findAllByOrderByFirstNameAsc();
+        assertEquals(employeeRepository.findAllByOrderByFirstNameAsc(),
+                expected.stream().sorted(Comparator.comparing(Employee::getFirstName))
+                        .collect(Collectors.toList()));
     }
 }
