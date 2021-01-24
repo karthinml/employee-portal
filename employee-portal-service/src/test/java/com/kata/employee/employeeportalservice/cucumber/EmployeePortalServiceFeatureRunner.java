8464f2ed67996c1943785bad3ebd1db5eed3bc70
Karthik Ramasamy <karthinml@gmail.com>
2021-01-24 11:37:18 +0530

    ListEmployee: Committing code for integration test, scenarios and stepdefs

--- employee-portal-service/src/test/java/com/kata/employee/employeeportalservice/cucumber/EmployeePortalServiceFeatureRunner.java
+++ employee-portal-service/src/test/java/com/kata/employee/employeeportalservice/cucumber/EmployeePortalServiceFeatureRunner.java
@@ -6,7 +6,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = {"com.kata.employee.employeeportalservice.cucumber.setpdefs"},
+@CucumberOptions(features = "src/test/resources/features",
+        //glue = {"com.kata.employee.employeeportalservice.cucumber.setpdefs"}, //TODO: Resolve path issue
         tags = "not @Ignore")
 public class EmployeePortalServiceFeatureRunner {
 }
--- employee-portal-service/src/test/java/com/kata/employee/employeeportalservice/cucumber/SpringIntegrationTest.java
+++ employee-portal-service/src/test/java/com/kata/employee/employeeportalservice/cucumber/SpringIntegrationTest.java
@@ -5,11 +5,14 @@ import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
 
-@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = EmployeePortalServiceApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class SpringIntegrationTest {
 
+    protected RestTemplate restTemplate = new RestTemplate();
+
+    protected String EMPLOYEE_SERVICE_URL = "http://localhost:8080/api/v1/employees";
 }
